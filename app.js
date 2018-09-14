7fb3d4685a14931f5d2fc28e188893e66715df5d
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-09-14 08:11:45 -0600

    Title box and Font added

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // const mashapeKey = `oaQ37rOdEymshQuTchX0YcpHvg57p1rIczVjsn1LeIL3QWibs8`
+  // let name = `leeroy`
+  // axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards', {
+  //     headers: { "X-Mashape-Key": `${mashapeKey}` }
+  //   })
+  //     .then((response) => {
+  //       console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+
+  function createForm() {
+    
+  }
+  })
